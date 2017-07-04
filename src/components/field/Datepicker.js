1be199f1c0d576db59485527551c4952f4e98c9a
Marvin Ruppelt <marvinruppelt@Marvins-MacBook-Pro.fritz.box>
2017-07-04 12:04:20 +0200

    WIP40-523

--- src/components/field/Datepicker.js
+++ src/components/field/Datepicker.js
@@ -4,41 +4,80 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import Datetime from 'react-datetime'
+import Picker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
+
 import moment from 'moment'
 import locale from 'moment/locale/de'
-import {Field} from 'redux-form'
-import styles from 'react-datetime/css/react-datetime.css'
 
-const Datepicker = ({input: {name, value, ...input}, disabled, ...rest}) => {
-    const format = "DD.MM.YYYY";
-
-    const onChange = function (a) {
-        if (moment(a, format, true).isValid() || a === null || a === "") {
-            return input.onChange(a);
+const Datepicker = ({input: {name, value, ...input}, disabled, dateFormat, openToDate, className, ...rest}) => {
+    const onChange = function (v) {
+        if (moment.isMoment(v) && v.isValid()) {
+            return input.onBlur(v);
         }
+
+        if (moment.utc(v, dateFormat, true).isValid() || v === null || v === "") {
+            return input.onChange(v);
+        }
+
+        return input.onChange('');
     };
 
-    value = value ? moment.utc(value) : null;
+    const onBlur = (event) => {
+        // const eTarget = event.target;
+        const eValue = event.target.value;
+        const v = moment.utc(eValue, dateFormat, true);
+
+        if (moment.isMoment(v) && v.isValid()) {
+            return input.onBlur(v);
+        }
+
+        return input.onBlur(eValue);
+    };
+
+    value = value
+        ? moment.utc(value, [dateFormat, 'YYYY-MM-DD'], true)
+        : null;
+    value = (moment.isMoment(value) && value.isValid())
+        ? value
+        : null;
 
     if (disabled) {
         return <div className="input-lookalike">
-            {moment(value).format(format)}
+            {moment.utc(value, [dateFormat, 'YYYY-MM-DD'], true).format(dateFormat)}
         </div>;
     }
 
-    return <Datetime
-        name={name}
-        value={value}
-        dateFormat={format}
-        timeFormat={false}
-        utc={true}
-        closeOnSelect={true}
-        selected={value}
-        onChange={onChange}
-        onBlur={input.onChange}
-        {...rest}
-    />;
+    const props = {
+        name,
+        value,
+        className,
+        dateFormat,
+        selected: value,
+        onChange,
+        onBlur,
+        ...rest
+    };
+
+    if (openToDate !== null) {
+        props.openToDate = (moment.isMoment(value) && value.isValid())
+            ? value
+            : moment.utc(openToDate, [dateFormat, 'YYYY-MM-DD'], true);
+    }
+
+    return <Picker {...props}/>;
+};
+
+Datepicker.defaultProps = {
+    className: 'form-control',
+    dateFormat: 'DD.MM.YYYY',
+    openToDate: null
+};
+
+Datepicker.propTypes = {
+    className: PropTypes.string,
+    dateFormat: PropTypes.string,
+    openToDate: PropTypes.string
 };
 
 export default Datepicker;
