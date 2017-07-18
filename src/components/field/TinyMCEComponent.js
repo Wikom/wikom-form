29dc84e3a8337f24194def57f94688b0dddb83cf
rouvenWikom <r.diener@wikom.de>
2017-07-18 16:07:54 +0200

    inline mode for tinyMCE component

--- src/components/field/TinyMCEComponent.js
+++ src/components/field/TinyMCEComponent.js
@@ -6,23 +6,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TinyMCE from 'react-tinymce'
 
-export default (props) => {
-    if(props.disabled){
-        return  <div
-                dangerouslySetInnerHTML={{__html: props.input.value}}
+const TinyMCEComponent = ({input: {name, value, ...input}, meta, disabled, inline, ...props}) => {
+
+    if (disabled === true) {
+        return (
+            <div
+                dangerouslySetInnerHTML={{__html: value}}
                 className="input-lookalike"
-            />;
+            />
+        );
     }
 
     return (
         <TinyMCE
             {...props}
-            content={props.input.value}
-            onBlur={(e) => props.input.onBlur(e.target.getContent())}
+            name={name}
+            content={value}
+            onBlur={(e) => input.onBlur(e.target.getContent())}
             config={{
+                inline: inline,
                 menubar: false,
                 toolbar: 'bold italic underline | alignleft aligncenter alignright ',
                 height: 150,
                 statusbar: true,
-            }}/>)
+            }}
+        />
+    );
 };
+
+TinyMCEComponent.defaultProps = {
+    disabled: false,
+    inline: false
+};
+
+export default TinyMCEComponent
