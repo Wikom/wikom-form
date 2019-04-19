dbb8e15a015984e905c625e2d24b445146f2a7ca
rouvenWikom <r.diener@wikom.de>
2019-04-19 08:17:17 +0200

    Anpassung an redux-form: Initiale Validierung nach vollständigem Mount, formErrors nach unmount Bereinigen

--- src/reducers/formErrorsReducer.js
+++ src/reducers/formErrorsReducer.js
@@ -3,11 +3,17 @@
  */
 
 import {FORM_ERROR_SET} from "../actions/actionTypes";
+import {actionTypes} from 'redux-form';
 
 const formErrorsReducer = (state = {}, action) => {
      switch (action.type) {
          case FORM_ERROR_SET:
              return Object.assign({}, state, {[action.name]: action.formErrors});
+         case actionTypes.DESTROY:
+             if (Object.keys(state).indexOf(action.meta.form[0]) !== -1) {
+                 const {[action.meta.form[0]]: _, ...nextState} = state;
+                 return nextState;
+             }
         default:
             return state;
     }
